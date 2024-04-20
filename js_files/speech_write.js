cf9ff4e7c9b001ab4077f1e7f6095f157bd8f59d
SninaH <ns5425@student.uni-lj.si>
2024-04-20 23:44:45 +0200

    speech recognition upgrade

--- js_files/speech_write.js
+++ js_files/speech_write.js
@@ -1,4 +1,8 @@
-const spoken_result = document.getElementById('spoken_result');
+
+
+const spoken_result = document.getElementById('spoken_result'); //text area v katerega vpisujemo 
+const JSONfileName = "image"
+var field_idx = 0
 
 function init() {
     window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -9,22 +13,32 @@ function init() {
         recognition: new window.SpeechRecognition(),
         text: ''
       }
-      speech.recognition.continuous = true;
-      speech.recognition.interimResults = true;
-      speech.recognition.lang = 'sl-SI';
-      //result napise kaj trenutno slisi
+      speech.recognition.continuous = true; // To allow to continously listen
+      speech.recognition.interimResults = true; // To return interim results to a transcript area
+      speech.recognition.lang = 'sl-SI'; // language set to slovenian
+      
+      //The result event of the Web Speech API is fired when the speech recognition service returns a result — a word or phrase has been positively recognized and this has been communicated back to the app
       speech.recognition.addEventListener('result', (event) => {
         const audio = event.results[event.results.length - 1];
         speech.text = audio[0].transcript;
+        console.log(speech.text);
+        
         const tag = document.activeElement.nodeName;
-        if (tag === 'INPUT' || tag === 'TEXTAREA') {
-          if (audio.isFinal) {
-            document.activeElement.value += speech.text;
-          }
-        }
         if (audio.isFinal){
-            spoken_result.innerText += speech.text
-            if (speech.text == "izbriši")
+            if (tag === 'INPUT' || tag === 'TEXTAREA') {
+            
+                document.activeElement.value += speech.text;
+            
+            }
+            
+                spoken_result.value += speech.text
+                console.log(spoken_result.value)
+                console.log("##############################################                  ")
+                if (speech.text.search("izbriši") > -1){
+                    console.log("IZBRISI TEXT");
+                    spoken_result.innerText = "";
+                    console.log(audio);
+                }
         }
         
         result.innerText = speech.text;
@@ -45,4 +59,19 @@ function init() {
       })
     }
   }
-  init();
+
+function loadQuestion(field_idx){
+    field_name = json_data[field_idx].field_name;
+    answer_type = json_data[field_idx].expected_answer_type; // None (vpisi text) ali pa list[str] (izberi moznost)
+
+    console.log(field_name, answer_type);
+
+    return [field_name, answer_type]; 
+}
+
+function addAnswerJSON(field_idx, answer){
+    json_data[field_idx].answer = answer;
+}
+
+
+init();
